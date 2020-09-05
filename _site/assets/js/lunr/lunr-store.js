2d2c6ca007a219944d8e5aa1082bd518ae16d88a
Sangyong Han <bany.abledo@gmail.com>
2020-09-05 15:41:36 +0900

    Update pages and _config.yml

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "github.io 블로그 시작하기",
+        "excerpt":"GitHub Blog 서비스인 github.io 블로그 시작하기로 했습니다.  이곳에 읽은 것 및 생각나고 배워나가는 모든 것을 쓰고 공유할 예정입니다.   Ruby 및 Jekyll도 배워가며 점점 업데이트를 하겠습니다.  ","categories": ["Blog"],
+        "tags": ["Blog"],
+        "url": "http://localhost:4000/blog/first-post/",
+        "teaser": null
+      }]
